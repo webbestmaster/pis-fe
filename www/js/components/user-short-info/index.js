d3d207851771ba98c9748844f59b2220e24be78e
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-01-18 14:46:17 +0300

    user page - in progress...

--- www/js/components/user-short-info/index.js
+++ www/js/components/user-short-info/index.js
@@ -10,7 +10,36 @@ class UserShortInfo extends Component {
         const view = this;
         const {props, state} = view;
 
-        return <h1 className={style.main_block}>{JSON.stringify(props.auth)}</h1>;
+        return <div className={style.main_block}>
+            <div className={style.user_avatar}>
+                <div className={style.user_avatar_image}
+                    style={{backgroundImage: 'url(' + 'http://via.placeholder.com/136x136' + ')'}}/>
+                <div className={style.user_avatar__edit_button}/>
+            </div>
+            <h4 className={style.user_avatar__name}>Антон Чехов</h4>
+            <h3 className={style.user_avatar__email}>long.user.email@the.best.email.com</h3>
+            <div className={style.user_avatar__logout_button}>
+                Выход<span className={style.user_avatar__logout_icon}/>
+            </div>
+
+            <div className={style.extra_info_wrapper}>
+                <div className={style.extra_info_block}>
+                    <p className={style.extra_info_value}>12</p>
+                    <p className={style.extra_info_key}>Бонусы</p>
+                </div>
+                <div className={style.extra_info_block}>
+                    <p className={style.extra_info_value}>03</p>
+                    <p className={style.extra_info_key}>Предстоящие</p>
+                </div>
+                <div className={style.extra_info_block}>
+                    <p className={style.extra_info_value}>02</p>
+                    <p className={style.extra_info_key}>Завершённые</p>
+                </div>
+            </div>
+            <div className="hidden">
+                {JSON.stringify(props.auth)}
+            </div>
+        </div>;
     }
 }
 
