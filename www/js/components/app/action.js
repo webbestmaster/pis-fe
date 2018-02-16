306aad47a799716928816b9edd6cf99421ffab6c
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-02-16 22:42:08 +0300

    fix for edge

--- www/js/components/app/action.js
+++ www/js/components/app/action.js
@@ -2,8 +2,8 @@
 const appConst = require('./const.json');
 
 export function scrollToTop() {
-    window.scrollTo(0, 0);
-    window.document.body.scrollTo(0, 0);
+    window.scrollTop = 0;
+    window.document.body.scrollTo = 0;
 
     return {
         type: appConst.type.scrollToTop,
