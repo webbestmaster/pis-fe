e7a47116d46884993b279da20295f1b06284d7fe
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-02-25 18:37:36 +0300

    vk login - flowjs

--- flow-typed/vk.js
+++ flow-typed/vk.js
@@ -1,6 +1,19 @@
-type TypeVk ={
-    init: Function;
-    Api: any
+type vkAuth = {
+    getLoginStatus(callback: Function): void;
+    getLoginStatus(callback: Function): void;
+}
+
+type TypeVk = {
+    init(params: { apiId: number }): void;
+    Auth: vkAuth;
+    Api: {
+        call(methodName: string,
+             methodParams: {
+                 user_ids: string | number;
+                 fields: string;
+             },
+             callback: Function): void
+    }
 };
 
 declare var VK: TypeVk;
