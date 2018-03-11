011530deeda146fdd313888aae094a56cbf23ee4
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-03-11 14:44:08 +0300

    bridge with BE - in progress...

--- www/js/blog/page/404/index.js
+++ www/js/blog/page/404/index.js
@@ -6,12 +6,12 @@ import TopBanner from './../../components/top-banner';
 import Footer from './../../components/footer';
 import Header from './../../components/header';
 import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+// import {connect} from 'react-redux';
 import style from './../../../page/404/style.m.scss';
 
 const image404 = require('./../../../page/404/images/404.png');
 
-class NotFoundPage extends Component<{}> {
+export default class NotFoundPage extends Component<{}> {
     componentDidMount() {
         metaTagMaster.updateByUrl('/page-404');
     }
@@ -33,6 +33,7 @@ class NotFoundPage extends Component<{}> {
     }
 }
 
+/*
 export default connect(
     state => ({
         // app: state.app,
@@ -40,4 +41,4 @@ export default connect(
     }),
     {}
 )(NotFoundPage);
-
+*/
