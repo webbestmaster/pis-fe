48d4c40fe03e70c3caa83f064b4a68d795ea840d
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-03-11 12:54:33 +0300

    css

--- www/js/blog/components/promo-article-text-3/index.js
+++ www/js/blog/components/promo-article-text-3/index.js
@@ -27,6 +27,8 @@ export default class PromoArticleText3 extends Component<{}> {
 
                     <p className={style.text}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
