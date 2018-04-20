d84fc8cc88d6263722ebc39e1331569aca7ec452
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-04-20 11:25:26 +0300

    update contacts page

--- www/js/page/contatcs/index.js
+++ www/js/page/contatcs/index.js
@@ -31,11 +31,13 @@ class ContactsPage extends Component {
             <div className="hug hug--simple-page">
                 <h3 className="section__header">Связаться с нами:</h3>
                 <div className="section__text-wrapper">
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
+                    <p>
+                        ООО «АтлантСпорт»<br/>
+                        220035 г.Минск, ул.Игнатенко, 4, 3 этаж, оф. 303<br/>
+                        Р/с BY47PJCB30120553771000000933 BYN<br/>
+                        ОАО «Приорбанк» ЦБУ №101 БИК PJCBBY2X<br/>
+                        УНП 193049971 ОКПО 501645925000
+                    </p>
                     <p>Телефон:&nbsp;&nbsp;<a href="tel:375291882699">+375 (29) 188-26-99</a></p>
                     <p>E-mail:&nbsp;&nbsp;<a href="mailto:admin@pis.by">admin@pis.by</a></p>
                 </div>
