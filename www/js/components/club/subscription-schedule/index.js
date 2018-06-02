cdd67804048c8269062daefaa2beab6c4dc9f913
webbestmaster <web.best.master@gmail.com>
2018-06-02 19:18:13 +0300

    table for mobile - done

--- www/js/components/club/subscription-schedule/index.js
+++ www/js/components/club/subscription-schedule/index.js
@@ -7,6 +7,7 @@ import {resolveImagePath} from './../../../helper/path-x';
 import {connect} from 'react-redux';
 import * as authAction from './../../auth/action';
 import style from './style.m.scss';
+import {fetchX} from './../../../helper/fetch-x';
 
 const appConst = require('./../../../app-const');
 const find = require('lodash/find');
@@ -18,6 +19,7 @@ import {getCategoryNameFromRow} from './../../trainings-catalog/helper';
 
 const partList = [
     {
+        id: 'personal',
         name: 'Персональное занятие',
         filter: subscription => {
             const neededCategoryName = 'gym';
@@ -32,6 +34,7 @@ const partList = [
         }
     },
     {
+        id: 'gym',
         name: 'Тренажерный зал',
         filter: subscription => {
             const neededCategoryName = 'gym';
@@ -46,6 +49,7 @@ const partList = [
         }
     },
     {
+        id: 'fitness',
         name: 'Фитнес',
         filter: subscription => {
             const excludedCategoryList = ['gym', 'after'];
@@ -55,6 +59,7 @@ const partList = [
         }
     },
     {
+        id: 'after',
         name: 'После спорта',
         filter: subscription => {
             const neededCategoryName = 'after';
@@ -72,10 +77,27 @@ class SubscriptionsSchedule extends Component {
         const view = this;
 
         view.state = {
-            // selectedDayIndex: view.getFirstActiveTrainingDay(),
-            // selectedTimeIndex: -1,
-            // openInfoPopupId: null
+            categoryMap: {
+                name: {}
+            }
         };
+    }
+
+    componentDidMount() {
+        const view = this;
+
+        fetchX(appConst.pageDataUrl.host + appConst.pageDataUrl.subscriptions).then(pageData => {
+            const mapName = {};
+
+            Object.keys(pageData.data.categoryFilter).forEach(categoryKey => {
+                mapName[categoryKey] = pageData.data.categoryFilter[categoryKey].title;
+            });
+
+            view.setState(prevState => {
+                prevState.categoryMap.name = mapName; // eslint-disable-line no-param-reassign
+                return prevState;
+            });
+        });
     }
 
     renderDesktop() {
@@ -94,6 +116,19 @@ class SubscriptionsSchedule extends Component {
 
                     if (subscriptionList.length === 0) {
                         return null;
+                    }
+
+                    if (part.id === 'fitness') {
+                        return <div className={style.part} key={part.name}>
+                            <div className={style.table_header}>
+                                <h3 className={style.table_header__name}>{part.name}</h3>
+                                <p className={style.table_header__time}>Время</p>
+                                <p className={style.table_header__cost}>Стоимость</p>
+                            </div>
+                            <div className={style.table_body}>
+                                {view.renderByCategoriesDesktop(subscriptionList)}
+                            </div>
+                        </div>;
                     }
 
                     return <div className={style.part} key={part.name}>
@@ -147,6 +182,17 @@ class SubscriptionsSchedule extends Component {
                         return null;
                     }
 
+                    if (part.id === 'fitness') {
+                        return <div className={style.part} key={part.name}>
+                            <div className={style.table_header}>
+                                <h3 className={style.table_header__name}>{part.name}</h3>
+                            </div>
+                            <div className={style.table_body}>
+                                {view.renderByCategoriesMobile(subscriptionList)}
+                            </div>
+                        </div>;
+                    }
+
                     return <div className={style.part} key={part.name}>
                         <div className={style.table_header}>
                             <h3 className={style.table_header__name}>{part.name}</h3>
@@ -178,6 +224,123 @@ class SubscriptionsSchedule extends Component {
                     </div>;
                 })}
         </div>;
+    }
+
+    getCategoryListByName(subscriptionList) {
+        const categoryList = [];
+
+        subscriptionList.forEach(subscription => {
+            const categoryName = getCategoryNameFromRow(subscription);
+
+            if (categoryList.indexOf(categoryName) === -1) {
+                categoryList.push(categoryName);
+            }
+        });
+
+        const categoryMap = {};
+
+        categoryList.forEach(categoryName => {
+            categoryMap[categoryName] = subscriptionList
+                .filter(subscription => getCategoryNameFromRow(subscription) === categoryName);
+        });
+
+        return categoryMap;
+    }
+
+    renderByCategoriesDesktop(subscriptionList) {
+        const view = this;
+        const {state} = view;
+        const {categoryMap} = state;
+        const subscriptionListByCategories = view.getCategoryListByName(subscriptionList);
+
+        return Object.keys(subscriptionListByCategories)
+            .map(categoryName => <div key={categoryName}>
+                <div className={style.table_body__line_header}>
+                    <h5 className={style.table_body__name}>{categoryMap.name[categoryName] || <span>&nbsp;</span>}</h5>
+                </div>
+
+                {subscriptionListByCategories[categoryName]
+                    .map(subscription => <Link
+                        to={'/subscription/' + subscription.id}
+                        key={subscription.id}
+                        className={style.table_body__line}>
+                        <div
+                            className={style.table_body__name}>
+                            {subscription.title}
+                        </div>
+                        <div
+                            className={style.table_body__time}>
+                            {reduceSeconds(subscription.work_from)}
+                            {' - '}
+                            {reduceSeconds(subscription.work_to)}
+                        </div>
+                        <div
+                            className={style.table_body__cost}>
+                            <span className="bold">{subscription.price}&nbsp;</span>руб.
+                        </div>
+                    </Link>)}
+            </div>);
+    }
+
+    renderByCategoriesMobile(subscriptionList) {
+        const view = this;
+        const {state} = view;
+        const {categoryMap} = state;
+        const subscriptionListByCategories = view.getCategoryListByName(subscriptionList);
+
+        return Object.keys(subscriptionListByCategories)
+            .map(categoryName => <div key={categoryName}>
+                <div className={style.table_body__line_header}>
+                    <h5 className={style.table_body__name}>{categoryMap.name[categoryName] || <span>&nbsp;</span>}</h5>
+                </div>
+
+                {subscriptionListByCategories[categoryName].map(subscription => <Link
+                    to={'/subscription/' + subscription.id}
+                    key={subscription.id}
+                    className={style.table_body__line}>
+                    <div
+                        className={style.table_body__name}>
+                        {subscription.title}
+                    </div>
+                    <div
+                        className={style.table_body__time}>
+                        <span className={style.table_body__label}>Время:</span>
+                        {reduceSeconds(subscription.work_from)}
+                        {' - '}
+                        {reduceSeconds(subscription.work_to)}
+                    </div>
+                    <div
+                        className={style.table_body__cost}>
+                        <span className={style.table_body__label}>Цена:</span>
+                        <span className="bold">{subscription.price}&nbsp;</span>руб.
+                    </div>
+                </Link>)}
+            </div>);
+
+        /*
+        return subscriptionList
+            .map(subscription => <Link
+                to={'/subscription/' + subscription.id}
+                key={subscription.id}
+                className={style.table_body__line}>
+                <div
+                    className={style.table_body__name}>
+                    {subscription.title}
+                </div>
+                <div
+                    className={style.table_body__time}>
+                    <span className={style.table_body__label}>Время:</span>
+                    {reduceSeconds(subscription.work_from)}
+                    {' - '}
+                    {reduceSeconds(subscription.work_to)}
+                </div>
+                <div
+                    className={style.table_body__cost}>
+                    <span className={style.table_body__label}>Цена:</span>
+                    <span className="bold">{subscription.price}&nbsp;</span>руб.
+                </div>
+            </Link>);
+*/
     }
 
     render() {
